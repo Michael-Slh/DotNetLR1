76d1368a039ff55fbcc0962551ea2ece48fd265f
Michael-Slh <actualhooman@gmail.com>
2022-06-28 19:17:55 +0300

    Add LR1

---
+++ Lab01/Program.cs
@@ -0,0 +1,196 @@
+﻿using Data;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var data = DataSeeding.GetData();
+            var queries = new Queries(data);
+            Options(queries);
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("Choose from 1 to 15 or 0 to quit.");
+            Console.WriteLine("1. Get first stop and quantity of buses driving from this point.");
+            Console.WriteLine("2. Get number of buses that run on routes with more than three buses.");
+            Console.WriteLine("3. Get companies with the biggest amount of routes.");
+            Console.WriteLine("4. Get first and last three buses.");
+            Console.WriteLine("5. Get routes with it\'s companies.");
+            Console.WriteLine("6. Get common routes between first and second buses.");
+            Console.WriteLine("7. Get list of the same amount of routes for buses.");
+            Console.WriteLine("8. Get car city codes with their amount.");
+            Console.WriteLine("9. Get all start and end points.");
+            Console.WriteLine("10. Get routes with the same amount of buses.");
+            Console.WriteLine("11. Get avarage amount of buses among routes.");
+            Console.WriteLine("12. Get all buses except buses on first and last route.");
+            Console.WriteLine("13. Get amount of symbols in routes.");
+            Console.WriteLine("14. Get all routes of companies.");
+            Console.WriteLine("15. Get all ordered bus numbers.");
+            Console.WriteLine("0. Quit.");
+        }
+
+        static void Options(Queries queries)
+        {
+            var finished = false;
+            while (!finished)
+            {
+                PrintMenu();
+                string? opt = Console.ReadLine();
+                if (opt is null || !int.TryParse(opt, out int result) || result < 0 || result > 15)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong option. Try again.");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    continue;
+                }
+                switch (result)
+                {
+                    case 0:
+                        Console.WriteLine("Program finished.");
+                        finished = true;
+                        break;
+                    case 1:
+                        {
+                            var res = queries.GetByFirstStopAndCount();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            var res = queries.GetThreeOrMoreBusses(3);
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            var res = queries.GetTopRoutesCompanies();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            var res = queries.GetFirstAndLastBusses();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            var res = queries.GetRouteCompany();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item.Name1 + " " + item.Name2);
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            var res = queries.GetCommonRoutes();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 7:
+                        {
+                            var res = queries.GetSameRoutes();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine("Amount: " + item.Name1 + ", buses: " + item.Name2);
+                            }
+                            break;
+                        }
+                    case 8:
+                        {
+                            var res = queries.GetNameplates();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item.Name1 + " - " + item.Name2);
+                            }
+                            break;
+                        }
+                    case 9:
+                        {
+                            var res = queries.GetRouteStartAndFinish();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item.Name);
+                            }
+                            break;
+                        }
+                    case 10:
+                        {
+                            var res = queries.GetRoutesByBusAmount();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine("Courses: " + item.Name2 + " with " + item.Name1);
+                            }
+                            break;
+                        }
+                    case 11:
+                        {
+                            var res = queries.GetAvgBussAmount();
+                            Console.WriteLine("Avarage amount: " + res);
+                            break;
+                        }
+                    case 12:
+                        {
+                            var res = queries.GetAllBussesButLastAndFirstRoute();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 13:
+                        {
+                            var res = queries.GetRoutesBySymbols();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine("Number of symbols: " + item.Name1 + ", routes: " + item.Name2);
+                            }
+                            break;
+                        }
+                    case 14:
+                        {
+                            var res = queries.GetRoutesByCompany();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine("Company \"" + item.Name1 + "\" has routes: " + item.Name2);
+                            }
+                            break;
+                        }
+                    case 15:
+                        {
+                            var res = queries.GetAllBusNumbers();
+                            foreach (var item in res)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                }
+
+                Console.Write("Press any button to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Lab01/Queries.cs
@@ -0,0 +1,205 @@
+﻿using Data;
+using Model;
+
+namespace Lab01
+{
+    public class Queries
+    {
+        private readonly Assets _data;
+
+        public Queries(Assets data)
+        {
+            _data = data;
+        }
+
+        // query
+        public IEnumerable<string> GetByFirstStopAndCount()
+        {
+            var queue = from route in _data.Routes
+                        group route by route.FirstStop.Id into routeGroup
+                        select new String
+                        (
+                            routeGroup.ElementAt(0).FirstStop.Name + " - " + routeGroup.Count()
+                        );
+            return queue;
+        }
+
+        public IOrderedEnumerable<string> GetThreeOrMoreBusses(int amount)
+        {
+            var queue = _data.Routes
+                .Where(route => route.BusAmount >= amount)
+                .SelectMany(route => route.Buses
+                    .Select(bus => bus.Number))
+                .OrderBy(num => num);
+            return queue;
+        }
+
+        // query
+        public IEnumerable<Company> GetTopRoutesCompanies()
+        {
+            var queue = (from companies in _data.Companies
+                         where companies.Courses.Count == (from max in _data.Companies
+                                                          select max.Courses.Count).Max()
+                         select companies);
+            return queue;
+        }
+
+        public IEnumerable<Bus> GetFirstAndLastBusses()
+        {
+            var queue = _data.Buses
+                .Take(3)
+                .Concat(_data.Buses
+                    .TakeLast(3));
+            return queue;
+        }
+
+        // query
+        public IEnumerable<StringString> GetRouteCompany()
+        {
+            var queue = from routes in _data.Routes
+                        join companies in _data.Companies
+                        on routes.Company.Id equals companies.Id
+                        select new StringString
+                        {
+                            Name1 = routes.Name,
+                            Name2 = companies.Name
+                        };
+            return queue;
+        }
+
+        public IEnumerable<string> GetCommonRoutes()
+        {
+            var queue = _data.Buses.ElementAt(1).Routes.Intersect(
+                _data.Buses
+                .ElementAt(2).Routes)
+                .Select(r => r.Name);
+            return queue;
+        }
+
+        public IEnumerable<StringString> GetSameRoutes()
+        {
+            var queue = _data.Buses.GroupBy(b => b.Routes.Count)
+                .Select(b => new StringString
+                {
+                    Name1 = b.Key.ToString(),
+                    Name2 = b.Select(n => n.Number)
+                    .Aggregate((n1, n2) => n1 + ", " + n2)
+                });
+            return queue;
+
+            Console.WriteLine("List of the same amount of routes for buses.\n");
+            foreach (var item in queue)
+            {
+                Console.WriteLine("Amount: " + item.Name1 + ", buses: " + item.Name2);
+            }
+        }
+
+        // query
+        public IEnumerable<StringString> GetNameplates()
+        {
+            var queue = from res in (from buses in _data.Buses
+                                     group buses by buses.Number[..2] into busesGroups
+                                     select new StringString
+                                     {
+                                         Name1 = busesGroups.Key,
+                                         Name2 = busesGroups.Count().ToString()
+                                     })
+                        orderby res.Name2 descending
+                        select res;
+            return queue;
+        }
+
+        public IOrderedEnumerable<BusStop> GetRouteStartAndFinish()
+        {
+            var queue = _data.Routes.Select(r => r.FirstStop)
+                .Union(_data.Routes.Select(r => r.LastStop))
+                .OrderBy(r => r.Name.Length);
+
+            return queue;
+        }
+
+        // query
+        public IEnumerable<StringString> GetRoutesByBusAmount()
+        {
+            var queue = from res in (from routes in _data.Routes
+                                     group routes by routes.BusAmount into routeGroups
+                                     select new StringString
+                                     {
+                                         Name1 = routeGroups.Key + " buses",
+                                         Name2 = (from r in routeGroups
+                                                  select r.Name).Aggregate((s1, s2) => s1 + ", " + s2)
+                                     })
+                        orderby res.Name1 descending
+                        select res;
+            return queue;
+        }
+
+        // query
+        public double GetAvgBussAmount()
+        {
+            var queue = (from amount in _data.Routes
+                         select amount.BusAmount).Average();
+
+            return queue;
+        }
+
+        // query
+        public IEnumerable<Bus> GetAllBussesButLastAndFirstRoute()
+        {
+            var queue = (from buses in _data.Buses
+                         select buses).Except((
+                            from buses1 in _data.Routes.First().Buses
+                            select buses1)
+                            .Union(
+                                from buses2 in _data.Routes.Last().Buses
+                                select buses2)
+                            );
+            return queue;
+        }
+
+        public IEnumerable<StringString> GetRoutesBySymbols()
+        {
+            var queue = _data.Routes.GroupBy(r =>
+                r.Name.Length)
+                .Select(r => new StringString
+                {
+                    Name1 = r.Key.ToString(),
+                    Name2 = r.Select(rr => rr.Name)
+                        .Aggregate((s1, s2) => s1 + ", " + s2)
+                });
+            return queue;
+        }
+
+        public IEnumerable<StringString> GetRoutesByCompany()
+        {
+            var queue = _data.Companies.Select(c =>
+                new StringString
+                {
+                    Name1 = c.Name,
+                    Name2 = c.Courses.Select(r => r.Name)
+                        .Aggregate((n1, n2) => n1 + ", " + n2)
+                }
+            );
+            return queue;
+        }
+
+        // query
+        public IOrderedEnumerable<Bus> GetAllBusNumbers()
+        {
+            var queue = from buses in _data.Buses
+                        orderby buses.Id
+                        select buses;
+
+            return queue;
+        }
+    }
+    public class StringString
+    {
+        public string Name1 { get; set; }
+        public string Name2 { get; set; }
+        public override string ToString()
+        {
+            return Name1 + " " + Name2;
+        }
+    }
+}
